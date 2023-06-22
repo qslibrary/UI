ef7c1de6a4916d9e3c083769807753d565cec0b2
jason <ranchengcheng@bilibili.com>
2023-06-22 17:03:36 +0800

    优化item view

--- app/src/main/java/com/shqiansha/ui/MainActivity.kt
+++ app/src/main/java/com/shqiansha/ui/MainActivity.kt
@@ -10,37 +10,49 @@ import com.shqiansha.ui.dialog.PictureSelectionDialog
 import com.shqiansha.ui.popup.OptionPopup
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity :AppCompatActivity(){
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         tvOption.setOnClickListener {
-            val option=OptionDialog()
-            option.setOnOptionSelectListener{
+            val option = OptionDialog()
+            option.setOnOptionSelectListener {
 
             }
             option.setValues(arrayOf("11")).setFragmentManager(supportFragmentManager).show()
         }
         tvConfirm.setOnClickListener {
-            val confirm=ConfirmationDialog()
-            confirm.title="titletitletitletitletitletitletitletitle"
-            confirm.hideCancel=true
+            val confirm = ConfirmationDialog()
+            confirm.title = "titletitletitletitletitletitletitletitle"
+            confirm.hideCancel = true
             confirm.show(supportFragmentManager)
         }
         tvPicture.setOnClickListener {
-            val picture=PictureSelectionDialog("com.shqiansha.ui.fileprovider")
-            picture.setOnPictureSelectedListener{
+            val picture = PictureSelectionDialog("com.shqiansha.ui.fileprovider")
+            picture.setOnPictureSelectedListener {
 
             }
             picture.show(supportFragmentManager)
         }
         tvPicker.setOnClickListener {
-            val picker=PickerDialog(arrayOf("1111","22222","33333","44444","55555","55556","555557","555558","555559"),1)
-            picker.title="请选择"
-            picker.show(supportFragmentManager,"picker")
+            val picker = PickerDialog(
+                arrayOf(
+                    "1111",
+                    "22222",
+                    "33333",
+                    "44444",
+                    "55555",
+                    "55556",
+                    "555557",
+                    "555558",
+                    "555559"
+                ), 1
+            )
+            picker.title = "请选择"
+            picker.show(supportFragmentManager, "picker")
         }
         poOption.setOnClickListener {
-            val popup=OptionPopup(poOption, arrayOf("1","2","3","4"),200)
+            val popup = OptionPopup(poOption, arrayOf("1", "2", "3", "4"), 200)
 //            popup.setItemGravity(Gravity.START)
             popup.setOnOptionSelectListener {
 
@@ -48,7 +60,7 @@ class MainActivity :AppCompatActivity(){
             popup.show()
         }
         uivTest.setOnClickListener {
-
+            uivTest.tvRight.text = "测试"
         }
     }
 }
--- ui/src/main/java/com/shqiansha/ui/view/ItemView.kt
+++ ui/src/main/java/com/shqiansha/ui/view/ItemView.kt
@@ -1,44 +1,24 @@
 package com.shqiansha.ui.view
 
-import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Color
 import android.util.AttributeSet
 import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.FrameLayout
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
-import androidx.core.view.marginStart
+import androidx.appcompat.content.res.AppCompatResources
 import com.shqiansha.ui.R
 import com.shqiansha.ui.util.SizeUtils
-import kotlinx.android.synthetic.main.view_item.view.*
 
 class ItemView : FrameLayout {
-    var leftText = ""
-    var leftTextSize = 14f
-    var leftTextColor: Int = 0
 
-
-    var rightText = ""
-    var rightTextSize = 14f
-    var rightTextColor: Int = RIGHT_TEXT_COLOR_DEFAULT
-
-    var rightImage = 0
-    var rightImageSize = 0f
-
-    var showRightText = false
-    var showRightImage = true
-    var showTopLine = false
-    var showBottomLine = false
-    var linePadding = 0f
-    var lineStartPadding = 0f
-    var lineEndPadding = 0f
-
-
+    lateinit var tvLeft: TextView
+    lateinit var tvRight: TextView
+    lateinit var ivRight: ImageView
 
     companion object {
         private val LEFT_TEXT_COLOR_DEFAULT = Color.parseColor("#333333")
@@ -53,49 +33,47 @@ class ItemView : FrameLayout {
         defStyleAttr
     ) {
         initAttr(attr)
-        initView()
     }
 
     private fun initAttr(attr: AttributeSet?) {
         val typedArray = context.obtainStyledAttributes(attr, R.styleable.ItemView)
-        leftText = typedArray.getString(R.styleable.ItemView_leftText) ?: ""
-        leftTextSize = typedArray.getDimension(R.styleable.ItemView_leftTextSize, 14f)
-        leftTextColor =
+        val leftText = typedArray.getString(R.styleable.ItemView_leftText) ?: ""
+        val leftTextSize = typedArray.getDimension(R.styleable.ItemView_leftTextSize, 14f)
+        val leftTextColor =
             typedArray.getColor(R.styleable.ItemView_leftTextColor, LEFT_TEXT_COLOR_DEFAULT)
 
-        rightText = typedArray.getString(R.styleable.ItemView_rightText) ?: ""
-        rightTextSize = typedArray.getDimension(R.styleable.ItemView_rightTextSize, 14f)
-        rightTextColor =
+        var rightText = typedArray.getString(R.styleable.ItemView_rightText) ?: ""
+        val rightTextSize = typedArray.getDimension(R.styleable.ItemView_rightTextSize, 14f)
+        val rightTextColor =
             typedArray.getColor(R.styleable.ItemView_rightTextColor, RIGHT_TEXT_COLOR_DEFAULT)
 
-        rightImage =
+        val rightImage =
             typedArray.getResourceId(R.styleable.ItemView_rightImage, R.drawable.ic_arrow_right_12)
-        rightImageSize = typedArray.getDimension(R.styleable.ItemView_rightImageSize, 12f)
+        val rightImageSize = typedArray.getDimension(R.styleable.ItemView_rightImageSize, 12f)
 
-        showRightText = typedArray.getBoolean(R.styleable.ItemView_showRightText, true)
-        showRightImage = typedArray.getBoolean(R.styleable.ItemView_showRightImage, true)
-        showTopLine = typedArray.getBoolean(R.styleable.ItemView_showTopLine, false)
-        showBottomLine = typedArray.getBoolean(R.styleable.ItemView_showBottomLine, false)
+        val showRightText = typedArray.getBoolean(R.styleable.ItemView_showRightText, true)
+        val showRightImage = typedArray.getBoolean(R.styleable.ItemView_showRightImage, true)
+        val showTopLine = typedArray.getBoolean(R.styleable.ItemView_showTopLine, false)
+        val showBottomLine = typedArray.getBoolean(R.styleable.ItemView_showBottomLine, false)
 
-        linePadding = typedArray.getDimension(R.styleable.ItemView_linePadding, 0f)
-        lineStartPadding = typedArray.getDimension(R.styleable.ItemView_lineStartPadding, 0f)
-        lineEndPadding = typedArray.getDimension(R.styleable.ItemView_lineEndPadding, 0f)
+        val linePadding = typedArray.getDimension(R.styleable.ItemView_linePadding, 0f)
+        var lineStartPadding = typedArray.getDimension(R.styleable.ItemView_lineStartPadding, 0f)
+        var lineEndPadding = typedArray.getDimension(R.styleable.ItemView_lineEndPadding, 0f)
         typedArray.recycle()
-    }
 
-    private fun initView() {
+
         val view = LayoutInflater.from(context).inflate(R.layout.view_item, this, false)
-        val tvLeft = view.findViewById<TextView>(R.id.tvItemLeft)
+        tvLeft = view.findViewById(R.id.tvItemLeft)
         tvLeft.text = leftText
         tvLeft.textSize = leftTextSize
         tvLeft.setTextColor(leftTextColor)
-        val tvRight = view.findViewById<TextView>(R.id.tvItemRight)
+        tvRight = view.findViewById(R.id.tvItemRight)
         if (!showRightText) rightText = ""
         tvRight.text = rightText
         tvRight.textSize = rightTextSize
         tvRight.setTextColor(rightTextColor)
 
-        val ivRight = view.findViewById<ImageView>(R.id.ivItemRight)
+        ivRight = view.findViewById(R.id.ivItemRight)
 
         if (showRightImage) {
             ivRight.visibility = View.VISIBLE
@@ -107,33 +85,29 @@ class ItemView : FrameLayout {
             ivRight.visibility = View.GONE
         }
 
-        val vTop=view.findViewById<View>(R.id.vTop)
-        val vBottom=view.findViewById<View>(R.id.vBottom)
+        val vTop = view.findViewById<View>(R.id.vTop)
+        val vBottom = view.findViewById<View>(R.id.vBottom)
 
-        vTop.visibility=if(showTopLine) View.VISIBLE else View.GONE
-        vBottom.visibility=if(showBottomLine) View.VISIBLE else View.GONE
+        vTop.visibility = if (showTopLine) View.VISIBLE else View.GONE
+        vBottom.visibility = if (showBottomLine) View.VISIBLE else View.GONE
 
-        if(lineStartPadding==0f)lineStartPadding=linePadding
-        if(lineEndPadding==0f) lineEndPadding=linePadding
-        val topLineParams= vTop.layoutParams as LinearLayout.LayoutParams
-        topLineParams.marginStart=lineStartPadding.toInt()
-        topLineParams.marginEnd=lineEndPadding.toInt()
+        if (lineStartPadding == 0f) lineStartPadding = linePadding
+        if (lineEndPadding == 0f) lineEndPadding = linePadding
+        val topLineParams = vTop.layoutParams as LinearLayout.LayoutParams
+        topLineParams.marginStart = lineStartPadding.toInt()
+        topLineParams.marginEnd = lineEndPadding.toInt()
 
-        val bottomLineParams= vBottom.layoutParams as LinearLayout.LayoutParams
-        bottomLineParams.marginStart=lineStartPadding.toInt()
-        bottomLineParams.marginEnd=lineEndPadding.toInt()
+        val bottomLineParams = vBottom.layoutParams as LinearLayout.LayoutParams
+        bottomLineParams.marginStart = lineStartPadding.toInt()
+        bottomLineParams.marginEnd = lineEndPadding.toInt()
         addView(view)
 
-        val backgroundValue=TypedValue()
-        context.theme.resolveAttribute(android.R.attr.selectableItemBackground,backgroundValue,true)
-        foreground=context.getDrawable(backgroundValue.resourceId)
-    }
-
-    fun getLeftTextView():TextView{
-        return tvItemLeft
-    }
-
-    fun getRightTextView():TextView{
-        return tvItemRight
+        val backgroundValue = TypedValue()
+        context.theme.resolveAttribute(
+            android.R.attr.selectableItemBackground,
+            backgroundValue,
+            true
+        )
+        foreground = AppCompatResources.getDrawable(context, backgroundValue.resourceId)
     }
 }
